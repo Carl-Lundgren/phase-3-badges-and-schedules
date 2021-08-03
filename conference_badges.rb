9bfd0a1fc925b21cfed5edaca582f3bc763dbc0f
Carl <lundgren.e.carl@gmail.com>
2021-08-03 12:15:18 -0700

    commit tree df9521fe80ba53dc2f1b1e15f8ccf7111c316da2

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
 # Write your code here.
+badge_maker(name)
+    "Hello, my name is #{name}"
+end
+
+batch_badge_maker(names)
+    names.each do |name|
+        "Hello, my name is #{name}"
+    end
+end
+
+assign_rooms(names)
+    counter = 1
+    names.each do |name|
+        "Hello, #{name}! You'll be assigned to room #{counter}!"
+        counter += 1
+    end
+end
+
+printer(names)
+    batch_badge_creator(names).each do |name|
+        puts name
+    end
+    assign_rooms(names).each do |name|
+        puts name
+    end
+end
